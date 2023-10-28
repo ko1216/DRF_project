4fa2ddff0f827c7d1659552c42c5831ebcc29df8
korchaginks <kkonstantinserg@gmail.com>
2023-10-29 00:31:55 +0300

    Изменил отображения уроков + добавил поле is_subscribed

--- main/serializers/course.py
+++ main/serializers/course.py
@@ -1,21 +1,20 @@
 from rest_framework import serializers
 
 from main.models import Course
+from main.serializers.lesson import LessonSerializer, LessonSerializerForCourse
 
 
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
-    lessons = serializers.SerializerMethodField()
+    lessons = LessonSerializerForCourse(many=True, read_only=True)
+    is_subscribed = serializers.BooleanField(read_only=True)
 
     class Meta:
         model = Course
-        fields = ['title', 'description', 'lessons_count', 'lessons']
+        fields = ['title', 'description', 'lessons_count', 'lessons', 'is_subscribed']
 
     def get_lessons_count(self, instance):
         if instance.lessons.all():
             return len(instance.lessons.all())
         else:
             return 0
-
-    def get_lessons(self, instance):
-        return [lesson.title for lesson in instance.lessons.all()]
