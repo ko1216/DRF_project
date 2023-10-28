616b985f502aa89caa90091b494f2e4daa747810
korchaginks <kkonstantinserg@gmail.com>
2023-10-29 00:28:54 +0300

    Добавил поле валидации в сериалайзере, а также сделал новый сериалайзер, в котором по умолчанию должен применяться контроллер прав на владельца

--- main/serializers/lesson.py
+++ main/serializers/lesson.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
 
 from main.models import Lesson
+from main.permissions import IsCourseOrLessonOwner
+from main.validators import VideoLinkValidator
 
 
 class LessonSerializer(serializers.ModelSerializer):
+    video_link = serializers.URLField(validators=[VideoLinkValidator('video_link')])
+
     class Meta:
         model = Lesson
         fields = '__all__'
+
+
+class LessonSerializerForCourse(serializers.ModelSerializer):
+
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+        permission_classes = [IsCourseOrLessonOwner]
