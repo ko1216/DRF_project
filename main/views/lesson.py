ac5eed4b6bff8456f3d5b78be586bb5ba2ef66e9
korchaginks <kkonstantinserg@gmail.com>
2023-10-29 00:27:19 +0300

    Добавил пагинацию в контроллер, а также явно передал сортировку по id уроков, нашел предупреждение об этом при выполнении тесткейсов

--- main/views/lesson.py
+++ main/views/lesson.py
@@ -2,6 +2,7 @@ from rest_framework.generics import RetrieveAPIView, ListAPIView, CreateAPIView,
 from rest_framework.permissions import IsAuthenticated
 
 from main.models import Lesson
+from main.pagination import SubjectsPagination
 from main.permissions import IsModerator, IsCourseOrLessonOwner, IsNotModerator
 from main.serializers.lesson import LessonSerializer
 from users.models import UserRoles
@@ -14,15 +15,26 @@ class LessonRetrieveAPIView(RetrieveAPIView):
 
 
 class LessonListAPIView(ListAPIView):
-    queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    pagination_class = SubjectsPagination
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
+        queryset = Lesson.objects.all()
+        ordering = self.request.query_params.get('ordering', 'id')
+
         if self.request.user.is_authenticated:
             if self.request.user.role == UserRoles.moderator:
-                return Lesson.objects.all()
+                return queryset.order_by(ordering)
             return Lesson.objects.filter(payments__user=self.request.user).distinct()
         return Lesson.objects.none()
+
+    def get(self, request, *args, **kwargs):
+        ordering = self.request.query_params.get('ordering', 'id')
+        queryset = self.get_queryset().order_by(ordering)
+        paginated_queryset = self.paginate_queryset(queryset)
+        serializer = LessonSerializer(paginated_queryset, many=True)
+        return self.get_paginated_response(serializer.data)
 
 
 class LessonCreateAPIView(CreateAPIView):
